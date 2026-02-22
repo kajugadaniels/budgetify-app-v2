2d985b701b23c42f6862244f0fcded4fcbe03820
Dvniels <kajugadaniels@gmail.com>
2026-02-22 14:28:14 +0200

    New changes

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from '@/components/shared/navbar'
 import React from 'react'
 
 const Dashboard = () => {
     return (
         <div>
+            <Navbar />
             Dashboard
         </div>
     )
